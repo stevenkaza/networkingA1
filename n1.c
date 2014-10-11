04e20a9bdd95e801f4ed1316645240f87bf3cdd5
skazavch <skazavch@mail.uoguelph.ca>
2014-10-11 18:47:27 -0400

    changed nothing

--- n1.c
+++ n1.c
@@ -7,18 +7,56 @@ Steven Kazavchinski
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
 #include <string.h>
 int main(int argc, char * argv[])
 {
+	char * buffer;   // for storing message contents from file  
+	char * nodeNumber = NULL; 
 	char * fileName = NULL; 
 	if (argv[1] != NULL)	
 	{
 		fileName = malloc(sizeof(char)*strlen(argv[1]+1));
 		strcpy(fileName,argv[1]); 
-		printf("%s\n",fileName); 
-
-
 	}
+	else
+	{
+		fprintf(stdout,"%s","You must specifcy a file name\n");	
+	}
+
+	if (argv[2]!=NULL)
+	{
+
+		nodeNumber = malloc(sizeof(char)*strlen(argv[2]+1)); 
+		strcpy(nodeNumber,argv[2]);
+	}
+        // read file 
+	readFile(fileName,buffer); 
+        	
+	// init pipe 
+}
+
+
+/* This function reads a file and copies its contents
+   into a buffer string 
+*/ 
+
+int readFile(char * fileName, char * buffer)
+{
+	FILE * fp; 
+        char c;
+
+	fp = fopen(fileName,"r");  
+	if (fp==NULL)
+		return 0; 
+	while(!feof(fp))
+	{
+		c = fgetc(fp);
+		if (c!=EOF) 		
+		    printf("%c",c); 
+	}
+	
 
 
 }
