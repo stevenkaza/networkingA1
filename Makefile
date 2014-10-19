8f6471d9e52d52fb2acf04e6e733826aa030cd74
stevenkaza1 <stevenkazav@gmail.com>
2014-10-19 20:41:43 +0000

    fixing

--- Makefile
+++ Makefile
@@ -1,18 +1,31 @@
-compileAll: n2
+all: n2
 	gcc n1.c -o node1
-n2:
+n2: n3
 	gcc n2.c -o node2
+n3: n4
+	gcc n3.c -o node3
 
-n3:
-	gcc n3.c
+n4: n5
+	gcc n4.c -o node4
 
-n4:
-	gcc n4.c
+n5: n6
+	gcc n5.c -o node5
 
-n5:
-	gcc n5.c
-
-n6:
-	gcc n6.c
+n6: n7
+	gcc n6.c -o node6
 n7:
-	gcc n7.c
+	gcc n7.c -o node7
+run: nex2
+	./node1
+nex2: nex3
+	./node2
+nex3: nex4
+	./node3
+nex4: nex5
+	./node4
+nex5: nex6
+	./node5
+nex6: nex7
+	./node6
+nex7:
+	./node7
---
+++ n3.c
@@ -0,0 +1,41 @@
+/* 
+cis 3210 A1
+0761977
+Steven Kazavchinski
+
+*/ 
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <limits.h>
+#include <string.h>
+int main(int argc, char * argv[])
+{
+
+	
+	
+        // read file 
+    redadPipe();
+
+	// init pipe 
+}
+
+
+
+int redadPipe(void)
+{
+    int res; 
+    char message[50];
+    res = open("n2",O_RDONLY);
+    
+    res = read(res,message,50);
+    printf("The message is %s\n",message);
+    
+}
+/* This function reads a file and copies its contents
+   into a buffer string 
+*/ 
